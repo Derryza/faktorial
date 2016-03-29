3e6267d6a58722e818293d9347ee2b516c349f92
Derry Jatnika <derry.jatnika@gmail.com>
2016-03-29 09:56:56 +0700

    Added files via upload

---
+++ faktorial.java
@@ -0,0 +1,57 @@
+package jurnal;
+
+import com.sun.xml.internal.bind.v2.schemagen.xmlschema.Import;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Praktikum
+ */
+public class faktorial {
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Masukan angka: ");
+        int baris=input.nextInt();;
+        for (int i = 0; i < baris; i++) {
+            
+            for (int j = baris; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            for (int j = baris; j > i; j--) {
+                System.out.print(" ");
+            }
+            System.out.println("");
+        }
+        for (int i = 0; i < baris; i++) {
+            
+            for (int j = 0; j <= i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = baris; j > i; j--) {
+                System.out.print("*");
+            }
+            for (int j = baris; j > i; j--) {
+                System.out.print("*");
+            }
+            for (int j = 0; j <= i; j++) {
+                System.out.print(" ");
+            }
+            System.out.println("");
+            
+            
+        }
+        
+    }
+    
+}
